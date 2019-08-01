073ed11695cfa48d77bc64a8b62ae946b101b021
Hajime <hajime0316@s.okayama-u.ac.jp>
2019-08-01 13:18:39 +0900

    Add feature which set tmp id

--- snake100_id_changer.ino
+++ snake100_id_changer.ino
@@ -1,4 +1,5 @@
 /* Minimum_Source*/
+#include "find_different_number.h"
 
 #define DYNAMIXEL_NUM 10
 #define ID_TABLE_SIZE (DYNAMIXEL_NUM * 2)
@@ -20,6 +21,20 @@ int id_table[ID_TABLE_SIZE] = {
 
 void setup() {
   // put your setup code here, to run once:
+
+  // すべてのDynamixelに一時IDを設定(ID変更時にIDが被らないようにするため)
+  for (int i = 0; i < DYNAMIXEL_NUM; i++)
+  {
+    int tmp_id;
+
+    // 一時ID決定
+    tmp_id = find_different_number(id_table, ID_TABLE_SIZE, 0, 254);
+    // 一時ID送信
+    Dxl.setID(PRESENT_ID_INDEX(i), tmp_id);
+    // IDテーブル更新
+    id_table[PRESENT_ID_INDEX(i)] = tmp_id;
+  }
+
 
 }
 
